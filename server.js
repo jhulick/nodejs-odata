ce98f7049418b80a4be84dbabf842eb221c67758
ide user ide_holgerkoser <holger.koser@gmail.com>
2012-04-23 06:38:43 +0000

    First comit

---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require('http'),
+    express = require('express'),
+    app = express.createServer(),
+    port = process.env.PORT;
+ 
+function home(req, res) {
+  res.redirect('/index.html');
+}
+
+function proxy(req, res) {
+    var odata_request = {
+      host: 'services.odata.org',
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+    odata_request.path = req.url;
+    http.get(odata_request, function (odata_response) {
+        res.header('Content-Type', 'application/json');
+        odata_response.pipe(res);
+    });
+}
+
+
+app.use(express.static(__dirname + '/static'));
+app.get('/', home);
+app.get('/Northwind/*', proxy);
+app.listen(port);
